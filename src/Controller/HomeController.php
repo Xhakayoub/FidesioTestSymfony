bed0ebfa552d6377a2c709a7c07c611eca957d9b
unknown <mansour.ismail.pro@gmail.com>
2020-02-15 07:50:01 +0100

    Front-End: create dynamic page for list filter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Gare;
 use App\Entity\Relation;
 use App\Entity\Ligne;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
@@ -67,7 +69,7 @@ class HomeController extends AbstractController
         ->findAll();
 
         $allLines = $this->getDoctrine()->getRepository(Ligne::class)
-        ->findAll();
+        ->findAll(array(),array('ligne' => 'ASC'));
         
         $Api = array();
 
@@ -100,7 +102,76 @@ class HomeController extends AbstractController
         return $this->render('home/dynamicHome.html.twig', [
             'controller_name' => 'HomeController',
             'gares' => $allStations,
-            'infos' => $Api
+            'infos' => $Api,
+            'lignes' =>  $allLines
         ]);
+    }
+
+
+
+
+
+
+
+
+   /**
+     * @Route("/dynamic/tofilter", name="filterList")
+     */
+    public function filter(request $request)
+    {   
+        $lineRequested = $request->request->get('line');
+    //     var_dump($requestedLine);
+    //     $response = new Response(json_encode(array(
+    //     'line' => $requestedLine,
+    // )));
+
+    //  return $response;
+         $allStations = $this->getDoctrine()->getRepository(Gare::class)
+         ->findAll();
+
+         $allLines = $this->getDoctrine()->getRepository(Ligne::class)
+         ->findAll(array(),array('ligne' => 'ASC'));
+
+          $allRelations = $this->getDoctrine()->getRepository(Relation::class)
+         ->findAll();
+        
+         $Api = array();
+
+        $relation = $this->getDoctrine()->getRepository(Relation::class)
+         ->findByLigne($lineRequested);
+         dump($relation);
+       foreach($relation as $rel)
+        {
+         
+         // $stationToChek = $rel->getGareId();
+          $lignes = array();
+        
+            foreach($allRelations as $relToCollect)
+             {  
+                 if($rel->getGareId() == $relToCollect->getGareId() && 
+                 $rel->getligne() != $relToCollect->getLigne()){
+                   
+                   array_push($lignes, $relToCollect->getLigne());
+                 }
+             }
+            
+             $obj = array('gare'=> $rel, 'lignes'=> $lignes);
+           //  var_dump($obj);
+             array_push($Api, $obj);
+
+         //     $allInfos = array([
+         //         "gare" => $station,
+         //         "lignes" => $lignes
+         //    ] );
+         //    array_push($Api,  $allInfos);
+
+         }
+
+         return $this->render('home/filteredList.html.twig', [
+             'controller_name' => 'HomeController',
+             'gares' => $allStations,
+             'infos' => $Api,
+             'lignes' =>  $allLines
+         ]);
     }
 }
